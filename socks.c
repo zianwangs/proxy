c100d81ff752e0f03eef6e1a19bb52ce61121d84
EC2 Default User <ec2-user@ip-172-31-36-129.us-east-2.compute.internal>
2021-11-12 12:55:04 +0000

    f

--- socks.c
+++ socks.c
@@ -1,6 +1,6 @@
 #include "csapp.h"
 #include "poll.h"
-#define MAX_READ (1 << 16)
+#define MAX_READ (1 << 17)
 
 int socks_authenticate(int, char*, char*);
 void socks_serve(int, int);
@@ -12,7 +12,7 @@ static void non_block_fd(int fd) {
 }
 
 int socks_authenticate(int fd, char* host, char* port) {
-    unsigned char buf[MAXLINE];
+    unsigned char buf[128];
     printf("Ready to receive socks5 handshake\n");
     int cnt = rio_readn(fd, buf, 3);
     printf("Handshake received, Ver:%x NM:%x M:%x\n", buf[0], buf[1], buf[2]);
@@ -20,7 +20,8 @@ int socks_authenticate(int fd, char* host, char* port) {
     char* handshake_reply = &NO_AUTHEN;
     rio_writen(fd, (char*)handshake_reply, 2);
     printf("Handshake replied, ready to process requests\n");
-    cnt = read(fd, buf, 64);
+    cnt = recv(fd, buf, 4, 0);
+    cnt += recv(fd, buf + 4, 64, 0);
     printf("Request received %d bytes\n",cnt);
     int dst_port = buf[cnt - 2] * 256 + buf[cnt - 1];
     sprintf(port, "%d", dst_port);
@@ -33,6 +34,7 @@ int socks_authenticate(int fd, char* host, char* port) {
         memcpy(host, buf + 5, cnt - 6);
         printf("DNAME: %s, Port:%d\n", host, dst_port);
     }
+    if (strcmp(host, "www.pornhub.com") == 0) return -1;
     int proxy_server_fd = open_clientfd(host, port);
     unsigned char SUCCESS_REPLY[10];
     SUCCESS_REPLY[0] = 5, SUCCESS_REPLY[1] = proxy_server_fd > 0 ? 0 : 4;
@@ -49,40 +51,30 @@ void socks_serve(int client_proxy_fd, int proxy_server_fd) {
     char buf[MAX_READ];
     int cnt1 = -1, cnt2 = -1;
     printf("Start passing data\n");
-    struct pollfd pfds[2];
-    non_block_fd(client_proxy_fd);
-    non_block_fd(proxy_server_fd);
+    // non_block_fd(client_proxy_fd);
+    // non_block_fd(proxy_server_fd);
+    struct pollfd pfds[2] = {0};
     pfds[0].fd = client_proxy_fd;
     pfds[1].fd = proxy_server_fd;
-    pfds[0].events = pfds[2].events = POLLIN;
-    //while (poll(&pfds, 2, -1) != -1) {
-        while (1) {
-            cnt1 = cnt2 = -2;
-            if ((cnt1 = recv(client_proxy_fd, buf, MAX_READ, MSG_WAITALL)) > 0) {
-                printf("Read %d bytes from client->proxy\n", cnt1);
-                if (rio_writen(proxy_server_fd, buf, cnt1) < 0) {
-                    break;
-                }
-                printf("Write %d bytes from proxy->server\n", cnt1);
-            }
-            if ((cnt1 == -1 || cnt1 == 0) && errno != EINTR && errno != EAGAIN && errno != EWOULDBLOCK) {
-                printf("%d\n", errno);
-                break;
-            }
-            if ((cnt2 = recv(proxy_server_fd, buf, MAX_READ, MSG_WAITALL)) > 0) {
-                printf("Read %d bytes from client->proxy\n", cnt2);
-                if (rio_writen(client_proxy_fd, buf, cnt2) < 0) {
-                    break;
-                }
-                printf("Write %d bytes from proxy->server\n", cnt2);
-            }
-            if ((cnt2 == -1 || cnt2 == 0) && errno != EINTR && errno != EAGAIN && errno != EWOULDBLOCK) {
-                printf("%d\n", errno);
-                break;
-            }
-            if (cnt1 == 0 && cnt2 == 0 && errno != EINTR && errno != EAGAIN && errno != EWOULDBLOCK) break;
-       // }
+    pfds[0].events = pfds[1].events = POLLIN;
+    while (poll(pfds, 2, -1) != -1) {
+        cnt1 = cnt2 = -1;
+        if (pfds[0].revents & POLLERR) break;
+        if (pfds[1].revents & POLLERR) break;
+        if (pfds[0].revents & POLLHUP) break;
+        if (pfds[1].revents & POLLHUP) break;
+        if (pfds[0].revents & POLLIN) {
+            cnt1 = recv(client_proxy_fd, buf, MAX_READ, 0);
+            if (cnt1 == 0) break;
+            send(proxy_server_fd, buf, cnt1, 0);
+        }
+        if (pfds[1].revents & POLLIN) {
+            cnt2 = recv(proxy_server_fd, buf, MAX_READ, 0);
+            send(client_proxy_fd, buf, cnt2, 0);
+        }
+        if (errno == ECONNRESET) break;
     }
+    // EINTR, EPIPE, EWOULDBLOCK, EAGAIN
     printf("Nothing to read from client, end session\n");
 
 }
